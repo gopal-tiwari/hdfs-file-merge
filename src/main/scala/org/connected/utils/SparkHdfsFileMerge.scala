41f17cf5c993c87d01cecd9275ce8d1a412d7d79
gopal <gopal.tiwari2012@gmail.com>
2019-05-28 19:43:37 +0530

    option d added

--- src/main/scala/org/connected/utils/SparkHdfsFileMerge.scala
+++ src/main/scala/org/connected/utils/SparkHdfsFileMerge.scala
@@ -4,12 +4,16 @@ import scopt.OptionParser
 
 object SparkHdfsFileMerge
 {
-  case class SparkFMConfig(database :String="", sourceTable:String="", partition:Option[String] = None, targetTable:String="")
+
+  case class SparkFMConfig(database: String = "", sourceTable: String = "", partition: Option[String] = None, targetTable: String = "")
 
   def main(args: Array[String]): Unit =
   {
-    val parser = new OptionParser[SparkFMConfig]( "SparkHdfsFileMerge") {
-
+    val parser = new OptionParser[SparkFMConfig]("SparkHdfsFileMerge")
+    {
+      opt[String]('d', "database")
+        .action((x, c) => c.copy(database = x)).required()
+        .text("Table which needs to be merged")
 
     }
   }
