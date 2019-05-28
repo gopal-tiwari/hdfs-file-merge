fb181017ea0927aec0139e1efb96076e63631a71
gopal <gopal.tiwari2012@gmail.com>
2019-05-28 19:41:07 +0530

    option parser created

--- src/main/scala/org/connected/utils/SparkHdfsFileMerge.scala
+++ src/main/scala/org/connected/utils/SparkHdfsFileMerge.scala
@@ -5,4 +5,13 @@ import scopt.OptionParser
 object SparkHdfsFileMerge
 {
   case class SparkFMConfig(database :String="", sourceTable:String="", partition:Option[String] = None, targetTable:String="")
+
+  def main(args: Array[String]): Unit =
+  {
+    val parser = new OptionParser[SparkFMConfig]( "SparkHdfsFileMerge") {
+
+
+    }
+  }
+
 }
